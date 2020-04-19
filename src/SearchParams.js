a0907cb39aa4a3a251b9dd42a22ab107def0e116
unknown <rlwee.russel@gmail.com>
2020-04-20 03:24:24 +0800

    react basics (April 20. 2020)

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,26 +1,46 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useContext } from "react";
 import pet, { ANIMALS } from "@frontendmasters/pet";
+
 import useDropdown from "./useDropdown";
+import Results from "./Results";
+import ThemeContext from "./ThemeContext";
 
 const SearchParams = () => {
   const [location, setLocation] = useState("Seattle, WA");
   const [breeds, setBreeds] = useState([]);
   const [animal, AnimalDropdown] = useDropdown("Animal", "dog", ANIMALS);
   const [breed, BreedDropdown, setBreed] = useDropdown("Breed", "", breeds);
+  const [pets, setPets] = useState([]);
+  const [theme, setTheme] = useContext(ThemeContext);
 
-  useEffect(() =>{
+  async function requestPets() {
+    const { animals } = await pet.animals({
+      location,
+      breed,
+      type: animal,
+    });
+
+    setPets(animals || []);
+  }
+
+  useEffect(() => {
     setBreeds([]);
     setBreed("");
 
-    pet.breeds(animal).then(({ breeds }) =>{
-      const breedStrings = breeds.map( ({name}) => name);
+    pet.breeds(animal).then(({ breeds }) => {
+      const breedStrings = breeds.map(({ name }) => name);
       setBreeds(breedStrings);
     }, console.error);
   }, [animal, setBreed, setBreeds]);
 
   return (
     <div className="search-params">
-      <form>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          requestPets();
+        }}
+      >
         <label htmlFor="Location">Location</label>
         <input
           id="location"
@@ -30,8 +50,26 @@ const SearchParams = () => {
         />
         <AnimalDropdown />
         <BreedDropdown />
-        <button>Submit</button>
+        <label htmlFor="theme">
+          Theme
+          <select
+            name=""
+            id=""
+            value={theme}
+            onChange={(e) => setTheme(e.target.value)}
+            onBlur={(e) => setTheme(e.target.value)}
+          >
+            <option value="peru">Peru</option>
+            <option value="darkblue">Dark Blue</option>
+            <option value="mediumorchid">Medium Orchid</option>
+            <option value="chartreuse">Chartreuse</option>
+          </select>
+        </label>
+        <button style={{ backgroundColor: theme }}>
+          Submit
+        </button>
       </form>
+      <Results pets={pets} />
     </div>
   );
 };
